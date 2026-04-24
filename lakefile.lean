9bb5af52e29f0437cbd998e8d304d2244f10971c
o-netzer <55181650+o-netzer@users.noreply.github.com>
2026-04-24 09:40:18 +0200

    Add files via upload

---
+++ lakefile.lean
@@ -0,0 +1,10 @@
+import Lake
+open Lake DSL
+
+-- Der Paketname (meist kleingeschrieben, mit Unterstrichen statt Leerzeichen)
+package "lean_proving" where
+  -- Hier könnten Einstellungen stehen
+
+-- Der Bibliotheksname (das, was man später mit 'import' aufruft)
+lean_lib «LeanProving» where
+  -- Hier könnten Einstellungen stehen
